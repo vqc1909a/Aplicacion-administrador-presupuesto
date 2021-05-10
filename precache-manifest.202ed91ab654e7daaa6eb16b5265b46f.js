1d6abd47bd6b70e75b8ea4e1ffa817d2852b8ea1
Nero Angel <neroangel1909@gmail.com>
2021-05-10 00:29:42 -0500

    Updates

---
+++ precache-manifest.202ed91ab654e7daaa6eb16b5265b46f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b81fed50f702a99482a1d26db108161",
+    "url": "/Aplicacion-administrador-presupuesto/index.html"
+  },
+  {
+    "revision": "496985c1c30c41e6838b",
+    "url": "/Aplicacion-administrador-presupuesto/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "8746831db3dd577ae7ef",
+    "url": "/Aplicacion-administrador-presupuesto/static/js/2.9c6b58e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Aplicacion-administrador-presupuesto/static/js/2.9c6b58e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496985c1c30c41e6838b",
+    "url": "/Aplicacion-administrador-presupuesto/static/js/main.d41387cb.chunk.js"
+  },
+  {
+    "revision": "d61e4d2d205cdf55ed8b",
+    "url": "/Aplicacion-administrador-presupuesto/static/js/runtime-main.73cd638f.js"
+  }
+]);
